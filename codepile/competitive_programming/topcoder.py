d7d91d5ff5c56e2f97aaa32be4bdabd62f101a3c
duypv1 <v.duypv1@vinai.io>
2022-09-24 15:51:48 +0000

    fix typo and update new process

--- codepile/competitive_programming/topcoder.py
+++ codepile/competitive_programming/topcoder.py
@@ -1,3 +1,4 @@
+from functools import cache
 import os
 import json
 import requests
@@ -7,10 +8,10 @@ from bs4 import BeautifulSoup
 from codepile.dataset import DatasetInfo, DatasetSources, RawDataset, Scraper, Processor, Analyser, Dataset
 
 
-TOPCODER_ARCHIVE_URL = "https://www.topcoder.com/tc?module=ProblemArchive&sr=0&er=10&sc=&sd=&class=&cat=&div1l=&div2l=&mind1s=&mind2s=&maxd1s=&maxd2s=&wr="
+TOPCODER_ARCHIVE_URL = "https://www.topcoder.com/tc?module=ProblemArchive&sr=0&er=10000&sc=&sd=&class=&cat=&div1l=&div2l=&mind1s=&mind2s=&maxd1s=&maxd2s=&wr="
 COOKIES_PATH = 'topcoder_cookies.json'
 HEADERS_PATH = 'topcoder_headers.json'
-
+# Get cookies from network in browers https://community.topcoder.com/stat?c=problem_solution&cr=14970299&rd=19420&pm=17819
 
 COOKIES = json.load(open(COOKIES_PATH, 'r'))
 HEADERS = json.load(open(HEADERS_PATH, 'r'))
@@ -78,18 +79,59 @@ class TopCoder(Scraper):
 
 
     def scrape(self) -> RawDataset:
-        all_url, all_name = self.get_all_problems()
+        total_url, total_name = self.get_all_problems()
         all_problem = []
         all_solution = []
-        for (i, url) in tqdm(enumerate(all_url), total=len(all_url)):
-            problem_statement, solutions = self.get_problem_solution(url)
+        all_url = []
+        all_name = []
+        failed_url = []
+        failed_name = []
+        if os.path.exists(os.path.join(self.target_dir, "topcoder.parquet")):
+            df_cache = pd.read_parquet(os.path.join(self.target_dir, "topcoder.parquet"))
+            cache_url = set(df_cache['url'].tolist())
+
+        for (i, (url, name)) in tqdm(enumerate(zip(total_url, total_name)), total=len(total_url)):
+            if url in cache_url:
+                continue
+            try:
+                problem_statement, solutions = self.get_problem_solution(url)
+                if len(solutions) == 0:
+                    failed_name.append(name)
+                    failed_url.append(url)
+                    print(f"Zero solution: {url}")
+            except Exception as error:
+                failed_url.append(url)
+                failed_name.append(name)
+                print(f"Failed to crawl: {url}")
+                print(f"Error: {error}")
+                continue
+
             all_problem.append(problem_statement)
             all_solution.append(solutions)
+            all_url.append(url)
+            all_name.append(name)
             if (i + 1) % 100 == 0:
                 df = pd.DataFrame({'problem': all_problem, 'solution': all_solution, 'url': all_url, 'name': all_name})
-                df.to_parquet(os.path.join(self.target_dir, 'topcoder.parquet'))
+                df.to_parquet(os.path.join(self.target_dir, 'topcoder_2.parquet'))
+        df_failed = pd.DataFrame({'url': failed_url, 'name': failed_name})
+        df_failed.to_parquet(os.path.join(self.target_dir, 'topcoder_failed.parquet'))
         df = pd.DataFrame({'problem': all_problem, 'solution': all_solution, 'url': all_url, 'name': all_name})
-        df.to_parquet(os.path.join(self.target_dir, 'topcoder.parquet'))
+
+        def preprocess_solution(text):
+            s = text.find('class="alignMiddle" ALIGN="left">') + len('class="alignMiddle" ALIGN="left">')
+            e = s + text[s:].find('</TD>')
+            return text[s:e]
+        
+        clean_sols = []
+        for lst in df.solution:
+            clean = []
+            for sol in lst.tolist():
+                temp = preprocess_solution(sol)
+                if temp != "":
+                    clean.append(temp)
+            clean_sols.append(clean)
+        df['clean_solution'] = clean_sols
+        df.to_parquet(os.path.join(self.target_dir, 'topcoder_2.parquet'))
         return RawDataset(storage_uris=['file:///{self.target_dir}'])
 
 
