ceb80a5622ce32580c358e80db0e8fed95ad62f7
Nathan Cooper <nacooper01@wm.edu>
2022-09-26 00:38:49 +0000

    Add CI testing and initial unit test

---
+++ tests/test_test.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        self.assertTrue(True)
+
+    def test_always_fails(self):
+        self.assertTrue(False)
