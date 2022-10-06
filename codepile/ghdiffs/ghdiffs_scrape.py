0177806e00b2aab3e948657aa966b43995488ca9
Herbie Bradley <mail@herbiebradley.com>
2022-10-07 00:41:26 +0100

    Fix file deletion bug

--- codepile/ghdiffs/ghdiffs_scrape.py
+++ codepile/ghdiffs/ghdiffs_scrape.py
@@ -1,14 +1,9 @@
 import json
 import urllib.request
 from pathlib import Path
-from pprint import pprint
-from xml.dom.minidom import ReadOnlySequentialNamedNodeMap
-
+import datetime
 import dask.bag as db
-import numpy as np
-import pyarrow as pa
-from codepile.dataset import (Analyser, Dataset, DatasetInfo, DatasetSources,
-                              Processor, RawDataset, Scraper)
+from codepile.dataset import (Dataset, DatasetInfo, RawDataset, Scraper)
 from dask.distributed import Client
 from unidiff import PatchSet
 
@@ -24,6 +19,7 @@ def process_ind_patch(patch_diff) -> dict:
     patch_parsed_diff["deletion_count"] = patch_diff.removed
     patch_parsed_diff["src_file"] = patch_diff.source_file
     patch_parsed_diff["tgt_file"] = patch_diff.target_file
+    patch_parsed_diff["file_extension"] = Path(patch_diff.target_file).suffix
     # patch_parsed_diff["patch_info"] = patch_diff.patch_info
     patch_diff_list = list(patch_diff)
     for patch_diff_ind in patch_diff_list:
@@ -40,14 +36,19 @@ def process_ind_patch(patch_diff) -> dict:
 
 def get_before_file(file_diff: dict, commit_hash: str, repo_name: str) -> str:
     repo_owner, repo_name = repo_name.split("/")
-    if file_diff["src_file"] == "/dev/null":
+    if file_diff["src_file"] == "/dev/null" or file_diff["tgt_file"] == "/dev/null":
         return ""
     # Get raw after file.
     file_raw_url = (f"https://raw.githubusercontent.com/{repo_owner}/"
                     f"{repo_name}/{commit_hash}/{file_diff['tgt_file'][2:]}")
-    raw_file = urllib.request.urlopen(file_raw_url)
-    raw_file_encoding = raw_file.headers.get_charsets()[0]
-    raw_file = [line.decode(raw_file_encoding) for line in raw_file.readlines()]
+    try:
+        raw_file = urllib.request.urlopen(file_raw_url)
+        raw_file_encoding = raw_file.headers.get_charsets()[0]
+        raw_file = [line.decode(raw_file_encoding) for line in raw_file.readlines()]
+    except Exception as e:
+        print(e)
+        print(file_raw_url)
+        return ""
     # file_length = sum(1 for _ in raw_file)
     # if file_length < length_threshold:
     # files_list.append(raw_file)
@@ -76,7 +77,10 @@ def process_commit(commit_data: dict) -> list[dict]:
     """
     # Scrape a commit's diff file.
     diff_url = f"https://github.com/{commit_data['repo_name']}/commit/{commit_data['commit']}.diff"
-    diff = urllib.request.urlopen(diff_url)
+    try:
+        diff = urllib.request.urlopen(diff_url)
+    except Exception as e:
+        return []
     encoding = diff.headers.get_charsets()[0]
     patch = PatchSet(diff, encoding=encoding)
     commit_list: list[dict] = []
@@ -102,13 +106,23 @@ class GitHubDiffDataset(Dataset):
     def process(self):
         raise NotImplementedError
 
+    @property
+    def info(self) -> DatasetInfo:
+        return DatasetInfo(
+            id="GitHubDiffDataset",
+            description="Dataset of diffs from GitHub")
+
+    @property
+    def id(self) -> str:
+        return ""
+
 
 class GitHubDiffScraper(Scraper):
     def __init__(self, config):
         # TODO: Dask multi-node scheduling here
-        client = Client(n_workers=config.n_workers, threads_per_worker=config.threads_per_worker)
-        self.read_path = Path(config.read_path)
-        self.save_path = Path(config.save_path)
+        client = Client(n_workers=16, threads_per_worker=1)
+        self.read_path = Path(config["read_path"])
+        self.save_path = Path(config["save_path"])
 
     def scrape(self) -> RawDataset:
         _ = (
@@ -116,12 +130,15 @@ class GitHubDiffScraper(Scraper):
             .map(process_commit).flatten().to_dataframe()
             .to_parquet(self.save_path)
         )
-        dataset = RawDataset(storage_uris=self.save_path, complete=True)
+        dataset = RawDataset(storage_uris=["https://github.com/CarperAI/Code-Pile"], complete=True)
         return dataset
 
 
 if __name__ == "__main__":
-    read_path = Path(__file__).parent / "test_file.json"
-    save_path = Path(__file__).parent / "test.parquet"
-    client = Client(n_workers=8, threads_per_worker=2)
-    db.read_text(read_path).map(json.loads).map(process_commit).flatten().to_dataframe().to_parquet(save_path)
+    read_path = Path(__file__).parent / "full_test.json"
+    save_path = Path(__file__).parent / "full_test.parquet"
+    config = {"read_path": read_path, "save_path": save_path}
+    ghdiff_dataset = GitHubDiffDataset(config)
+    ghdiff_dataset.download()
+    # client = Client(n_workers=16, threads_per_worker=1)
+    # db.read_text(read_path).map(json.loads).map(process_commit).flatten().to_dataframe().to_parquet(save_path)
