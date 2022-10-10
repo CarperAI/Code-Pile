b9f6b8a2c0c8ee0f171dd392c867de8e92ef921d
James Baicoianu <james_github@baicoianu.com>
2022-10-10 23:46:34 +0000

    Profiler support, set retry option based on crawl type

--- codepile/discourse/discourse.py
+++ codepile/discourse/discourse.py
@@ -2,22 +2,37 @@ from codepile.dataset import DatasetInfo, DatasetSources, RawDataset, Scraper, P
 from codepile.discourse.discourse_spider import DiscourseSummarySpider, DiscourseTopicSpider, generateCrawlSummary
 from scrapy.crawler import CrawlerProcess
 import os
+import pathlib
+
+import cProfile
 
 class DiscourseScraper(Scraper):
+    profiler_enabled = False
+
     def scrape(self) -> RawDataset:
+        if self.profiler_enabled:
+            profiler = cProfile.Profile()
+            profiler.enable()
+
+        pathlib.Path(self.target_dir).mkdir(parents=True, exist_ok=True)
         os.chdir(self.target_dir)
-        process = CrawlerProcess(settings={
+        crawlsettings = {
             "SCHEDULER_PRIORITY_QUEUE": 'scrapy.pqueues.DownloaderAwarePriorityQueue',
             "CONCURRENT_REQUESTS": 1000,
-            "LOG_LEVEL": "WARNING",
-            "RETRY_ENABLED": False # TODO - this should depend on crawl type
-        })
+            "LOG_LEVEL": "WARN",
+            "AUTOTHROTTLE_ENABLED": True,
+            "AUTOTHROTTLE_DEBUG": True,
+            "REACTOR_THREADPOOL_MAXSIZE": 50,
+            "JOBDIR": "scrapy-job",
+        }
 
         # TODO - crawl type should be an argument we can pass in
         crawltype = 'topics'
 
         if crawltype == 'summary':
             # use DiscourseSummarySpider to generate crawl summary by grabbing the index from every site
+            crawlsettings['RETRY_ENABLED'] = False
+            process = CrawlerProcess(crawlsettings)
             process.crawl(DiscourseSummarySpider)
             process.start()
 
@@ -25,8 +40,17 @@ class DiscourseScraper(Scraper):
             generateCrawlSummary()
         elif crawltype == 'topics':
             # use DiscourseTopicSpider to perform a full crawl
+            process = CrawlerProcess(crawlsettings)
             process.crawl(DiscourseTopicSpider)
             process.start()
+
+        if self.profiler_enabled:
+            profiler.disable()
+            print("write profile to profile.prof")
+            #with open('profile.txt', 'w') as fd:
+            #    pstat_profile = pstats.Stats(profiler, stream=fd)
+            #    pstat_profile.print_stats()
+            profiler.dump_stats("profile.prof")
 
         return RawDataset(storage_uris=['file:///{self.target_dir}'],
                 metadata='')
