8f700ea89b272cef76d8bfa38c27efdb0f9222ed
tranquoctrinh <tranquoctrinh.ai@gmail.com>
2022-10-11 18:04:59 +0000

    fix bug add data process with exact dedup

--- codepile/enwikibooks/data_process_pipeline/main.py
+++ codepile/enwikibooks/data_process_pipeline/main.py
@@ -1,54 +1,21 @@
 """Filtering."""
 
-from multiprocessing import cpu_count
 import os
 import logging
 
 import argparse
 
-from datasets import load_dataset, Dataset
+from datasets import Dataset
 import pandas as pd
 
 from filtering import DatasetFiltering
 
-from tqdm import tqdm
+from deduplicate import exact_deduplicate_dataset
+from utils import check_num_proc
 
 
 logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO").upper())
 logger = logging.getLogger(__name__)
-
-
-def check_num_proc(num_proc: int = -1) -> int:
-    """
-    Check the number of processors. Return a safe-checked value.
-
-    Parameters
-    ----------
-    num_proc : int, optional
-        Number of processors to use, by default -1
-
-    Returns
-    -------
-    int
-        Number of processors to use
-
-    Raises
-    ------
-    ValueError
-        If the input exceeds the number of processors available
-    """
-    maximum: int = cpu_count()
-    if num_proc > maximum:
-        raise ValueError(
-            f"{num_proc} exceeds the maximum number ({maximum}) of processors"
-        )
-
-    if num_proc == -1:
-        num_proc = maximum
-    else:
-        print(f"Using {num_proc} processors out of {maximum} can be slow")
-
-    return num_proc
 
 
 def parseArgs():
@@ -188,82 +155,10 @@ def main():
     dataset_filtering.modifying_documents()
     dataset_filtering.filtering()
     # save parquet
-    dataset_filtering.ds.to_pandas().to_parquet(os.path.join(args.output_dir, f"filtered_{args.data_file}"))
-    # dataset_filtering.save_dataset()
-
-
-# #---------------- Deduplication ----------------#
-    ds = dataset_filtering.ds
-
-    logger.info(f"Done preprocess {len(ds)} records")
-
-    if not os.path.exists(args.cache_dir):
-        os.makedirs(args.cache_dir, exist_ok=True)
-
-    #output_dir
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir, exist_ok=True)
-
-    urls = ds.map(
-        lambda x: {
-            "url" : x['url'],
-            "text": x["text"].replace("\n", " "),
-        },
-        num_proc=check_num_proc(args.num_proc),
-        desc="Extracting URLs",
-    )
-    logger.info(
-        f"Extracted URLs: {len(urls['url'])}, Unique URLs: {len(set(urls['url']))}"
-    )
-
-    # Save text data for substring deduplication
-    urls.to_csv(
-        os.path.join(args.output_dir, "text.csv"),
-        num_proc=check_num_proc(args.num_proc),
-        index=False,
-        header=False,
-        columns=["text"],
-    )
-    urls.to_csv(
-        os.path.join(args.output_dir, "ids.csv"),
-        num_proc=check_num_proc(args.num_proc),
-        index=False,
-        header=False,
-        columns=["id"],
-    )
-
-    del urls
-
-    logger.info(f"Start hashing {len(ds)} records")
-    if args.ignore_punctuation:
-        assert (
-            args.tokenization != "punctuation"
-        ), f"Cannot ignore punctuation when tokenization is set to `punctuation`"
-
-    ds = ds.map(
-        hashing,
-        fn_kwargs={
-            "tokenization": args.tokenization,
-            "window_size": args.window_size,
-            "column": args.text_column,
-            "ignore_punctuation": args.ignore_punctuation,
-            "lowercase": args.lowercase,
-            "output": INTERNAL_HASH,
-        },
-        num_proc=check_num_proc(args.num_proc),
-        desc="Hashing",
-    )
-    logger.info(f"Done hashing {len(ds)} records")
-
-    logger.info(f"Start querying {len(ds)} records")
-    matches = simhash.find_all(
-        tqdm(ds[INTERNAL_HASH], total=len(ds)),
-        args.num_blocks,
-        args.hamming_distance,
-    )
-    logger.info(f"Done querying {len(ds)} records, found {len(matches)} matches")
-
-    
+    out_file = os.path.join(args.output_dir, f"filtered_{args.data_file.split('/')[-1]}")
+    exact_deduplicate_dataset(dataset_filtering.ds, args)
+    logger.info("Saving filtered / deduplicated parquet at %s", out_file)
+    dataset_filtering.ds.to_pandas().to_parquet(out_file)
 
 if __name__ == "__main__":
     main()
