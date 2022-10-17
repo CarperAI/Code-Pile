ef100741a22fec7635f56de6982bbb93cf6725d2
Herbie Bradley <mail@herbiebradley.com>
2022-10-17 13:32:30 +0100

    Add filtering

--- codepile/ghdiffs/ghdiffs_scrape.py
+++ codepile/ghdiffs/ghdiffs_scrape.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 import urllib.request
 import argparse
 from pathlib import Path
@@ -36,10 +37,10 @@ def process_ind_patch(patch_diff) -> dict:
     return patch_parsed_diff
 
 
-def get_before_file(file_diff: dict, commit_hash: str, repo_name: str) -> str:
+def get_before_file(file_diff: dict, commit_hash: str, repo_name: str, length_threshold: int) -> str:
     repo_owner, repo_name = repo_name.split("/")
     if file_diff["src_file"] == "/dev/null":
-        raw_file = ["Add"]
+        raw_file: Any = ["ADDFILE"]
     elif file_diff["tgt_file"] == "/dev/null":
         # If file is deleted, get before file from the raw diff, which will be the full file.
         raw_file = [line[1:] + "\n" for line in file_diff["hunks_process"][0][3:]]
@@ -51,13 +52,12 @@ def get_before_file(file_diff: dict, commit_hash: str, repo_name: str) -> str:
             raw_file = urllib.request.urlopen(file_raw_url)
             raw_file_encoding = raw_file.headers.get_charsets()[0]
             raw_file = [line.decode(raw_file_encoding) for line in raw_file.readlines()]
+            if length_threshold > 0 and len(raw_file) > length_threshold:
+                return ""
         except Exception as e:
             print(e)
             print(file_raw_url)
             return ""
-        # file_length = sum(1 for _ in raw_file)
-        # if file_length < length_threshold:
-        # files_list.append(raw_file)
         # Iterate over hunks for this file and apply the reverse patch.
         for hunk in file_diff["hunks_process"]:
             hunk_list = []
@@ -67,39 +67,6 @@ def get_before_file(file_diff: dict, commit_hash: str, repo_name: str) -> str:
             raw_file[hunk[0][0] - 1:hunk[0][0] + hunk[1][1] - 1] = hunk_list
     del file_diff["hunks_process"]  # Deletes this item from the dict in parent functions
     return "".join(raw_file)
-
-
-def process_commit(commit_data: dict) -> list[dict]:
-    """
-    Process a commit dictionary to get the before files and diff dict.
-
-    Args:
-        commit_data (dict): Dictionary containing commit hash, repo name, and
-        commit message.
-
-    Returns:
-        list[dict]: A list of dicts, where each dict contains the data for a
-        change to a single file.
-    """
-    # Scrape a commit's diff file.
-    diff_url = f"https://github.com/{commit_data['repo_name']}/commit/{commit_data['commit']}.diff"
-    try:
-        diff = urllib.request.urlopen(diff_url)
-    except Exception as e:
-        print(e)
-        return []
-    encoding = diff.headers.get_charsets()[0]
-    patch = PatchSet(diff, encoding=encoding)
-    commit_list: list[dict] = []
-    for patch_ind in patch:
-        # Skip if the file is not a python file.
-        # if not Path(patch_ind.target_file).suffix == ".py":
-        #     continue
-        diff_dict: dict = process_ind_patch(patch_ind)
-        diff_dict.update(commit_data)
-        diff_dict["before_file"] = get_before_file(diff_dict, commit_data["commit"], commit_data["repo_name"])
-        commit_list.append(diff_dict)
-    return commit_list
 
 
 class GitHubDiffDataset(Dataset):
@@ -127,19 +94,64 @@ class GitHubDiffDataset(Dataset):
 class GitHubDiffScraper(Scraper):
     def __init__(self, config):
         # TODO: Dask multi-node scheduling here
-        self.client = Client(n_workers=config.n_workers, threads_per_worker=config.threads_per_worker)
+        # self.client = Client(n_workers=config.n_workers, threads_per_worker=config.threads_per_worker)
         self.read_path = Path(config.read_path)
         self.save_path = Path(config.save_path)
+        self.python_only = config.python_only
+        self.diff_length_threshold = config.diff_length_threshold
+        self.code_length_threshold = config.code_length_threshold
+        self.ignore_deletions = config.ignore_deletions
 
     def scrape(self) -> RawDataset:
+        # TODO: pass meta dataframe
         result = (
             db.read_text(self.read_path).map(json.loads)
-            .map(process_commit).flatten().to_dataframe()
+            .map(self.process_commit).flatten().to_dataframe()
             .to_parquet(self.save_path)
         )
         progress(result)
         dataset = RawDataset(storage_uris=["https://github.com/CarperAI/Code-Pile"], complete=True)
         return dataset
+
+    def process_commit(self, commit_data: dict) -> list[dict]:
+        """
+        Process a commit dictionary to get the before files and diff dict.
+
+        Args:
+            commit_data (dict): Dictionary containing commit hash, repo name, and
+            commit message.
+
+        Returns:
+            list[dict]: A list of dicts, where each dict contains the data for a
+            change to a single file.
+        """
+        if self.python_only and commit_data["language_name"] != "Python":
+            return []
+        # Scrape a commit's diff file.
+        diff_url = f"https://github.com/{commit_data['repo_name']}/commit/{commit_data['commit']}.diff"
+        try:
+            diff = urllib.request.urlopen(diff_url)
+            encoding = diff.headers.get_charsets()[0]
+            patch = PatchSet(diff, encoding=encoding)
+        except Exception as e:
+            print(e)
+            return []
+        commit_list: list[dict] = []
+        # Iterate over files within the diff.
+        for patch_ind in patch:
+            if self.ignore_deletions and patch_ind.target_file == "/dev/null":
+                continue
+            if self.diff_length_threshold > 0 and sum(len(hunk) for hunk in patch_ind) > self.diff_length_threshold:
+                continue
+            diff_dict: dict = process_ind_patch(patch_ind)
+            diff_dict["before_file"] = get_before_file(diff_dict, commit_data["commit"], commit_data["repo_name"],
+                                                       length_threshold=self.code_length_threshold)
+            if not diff_dict["before_file"]:
+                # Happens if exception is thrown or file is too long.
+                continue
+            diff_dict.update(commit_data)
+            commit_list.append(diff_dict)
+        return commit_list
 
 
 if __name__ == "__main__":
@@ -149,6 +161,13 @@ if __name__ == "__main__":
     parser.add_argument('--save_path', type=str)
     parser.add_argument('--n_workers', type=int, default=8)
     parser.add_argument('--threads_per_worker', type=int, default=2)
+    parser.add_argument('--python_only', type=bool, default=False)
+    parser.add_argument('--diff_length_threshold', type=int, default=1000,
+                        help="Maximum number of lines in the diff for a *single* file. Set to 0 for no limit.")
+    parser.add_argument('--code_length_threshold', type=int, default=1000,
+                        help="Maximum number of lines in code files")
+    parser.add_argument('--ignore_deletions', type=bool, default=True,
+                        help="Ignore file deletion diffs.")
     config = parser.parse_args()
     ghdiff_dataset = GitHubDiffDataset(config)
     ghdiff_dataset.download()
