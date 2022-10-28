bf85dcdc5b35f07b899135327f06edd6e1551f2d
Nathan Cooper <nacooper01@wm.edu>
2022-10-28 13:11:41 +0000

    Reorg leetcode tests to match others

--- codepile/leetcode/tests/test_leetcode.py
+++ codepile/leetcode/tests/test_leetcode.py
@@ -5,8 +5,11 @@ import pandas as pd
 from codepile.codepile import Config
 from codepile.leetcode.leetcode import LeetCodeDataset
 
+import pytest
+
 class TestLeetCodeDataset(TestCase):
-    def setUp(self):
+    @pytest.mark.s3_download
+    def test_same_cols(self):
         if not os.path.exists('data/'):
             os.mkdir('data/')
 
@@ -21,6 +24,4 @@ class TestLeetCodeDataset(TestCase):
 
         self.dummy = pd.read_parquet("test/leetcode_dummy.parquet")
         self.df = pd.read_parquet("data/leetcode_topics_with_questions.parquet.gzip")
-
-    def test_same_cols(self):
         self.assertEqual(self.dummy.columns.tolist(), self.df.columns.tolist())
