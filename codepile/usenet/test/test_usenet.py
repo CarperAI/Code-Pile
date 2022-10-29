40090d9e311a77c3ef113dd92ca00a00c48ba2b2
Akshay Home <jehoshaph.akshay@gmail.com>
2022-10-30 00:30:04 +0530

    Move the test file to the UseNet test folder

--- codepile/usenet/test/test_usenet.py
+++ codepile/usenet/test/test_usenet.py
@@ -9,10 +9,12 @@ import pyarrow.parquet as pq
 
 
 class TestUsenetDataset(TestCase):
+
     def setUp(self):
+        this_dir = os.path.dirname(__file__)
         output_data_dir = 'data/output/'
         raw_data_dir = 'data/raw/'
-        tmpdir = '/tmp/usenet/'
+        tmpdir = 'data/tmp/usenet/'
 
         if not os.path.exists(output_data_dir):
             os.makedirs(output_data_dir)
@@ -33,7 +35,7 @@ class TestUsenetDataset(TestCase):
         # Testing on two of the smaller archives
         usenet_dataset.download(files=['comp.ai.games.mbox.zip', 'comp.lang.basic.visual.mbox.zip', ])
 
-        self.test_data = pq.read_table('test/usenet-comp.parquet').to_pandas()
+        self.test_data = pq.read_table(os.path.join(this_dir, 'usenet-comp.parquet')).to_pandas()
         self.df = pq.read_table(os.path.join(output_data_dir, 'usenet-comp.parquet')).to_pandas()
 
     def test_same_cols(self):
